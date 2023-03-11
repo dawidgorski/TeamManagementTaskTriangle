6dfc9116f9805cbb56f821072078a69db7c46bbb
Dawid <dawidgorskimail.gmail.com>
2023-03-11 13:41:53 +0100

    Add SideLength() method

--- TeamManagementTaskTriangle/Program.cs
+++ TeamManagementTaskTriangle/Program.cs
@@ -11,6 +11,10 @@
         }
         return cleanInput1;
     }
+    public static double SideLength(double x1, double y1, double x2, double y2)
+    {
+        return Math.Sqrt(Math.Pow((x1 - x2), 2) + Math.Pow((y1 - y2), 2));
+    }
 
     public static void Main(string[] args)
     {
@@ -27,6 +31,16 @@
         double dot3x = ParseStringValue();
         Console.WriteLine("Enter coordinate y of dot C:");
         double dot3y = ParseStringValue();
+
+        double ab = SideLength(dot1x, dot1y, dot2x, dot2y);
+        double bc = SideLength(dot2x, dot2y, dot3x, dot3y);
+        double ac = SideLength(dot1x, dot1y, dot3x, dot3y);
+
+        Console.WriteLine($"Length of AB is: '{ab}'");
+        Console.WriteLine($"Length of BC is: '{bc}'");
+        Console.WriteLine($"Length of AC is: '{ac}'");
+
+        Console.WriteLine();
 
 
     }
